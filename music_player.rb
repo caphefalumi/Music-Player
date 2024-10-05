479c933f41e4a25789ea694c63c959244f8c90f0
Dangduytoan12l <dangduytoan13l@gmail.com>
2024-10-05 15:08:48 +0700

    Fix bugs where font are in low res

--- music_player.rb
+++ music_player.rb
@@ -19,13 +19,20 @@ end
 
 GENRE_NAMES = ['Null', 'Pop', 'Classic', 'Jazz', 'Rock']
 
-class UI 
-  attr_accessor :stop, :play, :next
+class UI
+  attr_accessor :stop, :play, :next, :sort_by, :pipe, :title, :genre, :date
 
   def initialize()
     @stop = Gosu::Image.new("assets/stop.png", false)
     @play = Gosu::Image.new("assets/play.png", false)
     @next = Gosu::Image.new("assets/next.png", false)
+
+    # Create text as images using Image::from_text
+    @sort_by = Gosu::Image.from_text("Sort By:", 250, {retro: true})
+    @pipe = Gosu::Image.from_text("|", 300, {retro: true})
+    @title = Gosu::Image.from_text("Title", 300, {retro: true})
+    @genre = Gosu::Image.from_text("Genre", 300, {retro: true})
+    @date = Gosu::Image.from_text("Date", 300, {retro: true})
   end
 end
 
@@ -58,10 +65,12 @@ class MusicPlayerMain < Gosu::Window
   def initialize
     super WIDTH, HEIGHT
     self.caption = "Music Player"
+    
+    @title_size = 250
+    @track_font_size = 180
+    @playing_track_font_size = 380
+    @sort_font_size = 300
 
-    @title = Gosu::Font.new(40)
-    @track_font = Gosu::Font.new(180)
-    @playing_track_font = Gosu::Font.new(380)
     @current_playing_track = nil
 		@album = nil
     @albums = load_albums()
@@ -74,13 +83,9 @@ class MusicPlayerMain < Gosu::Window
     @desired_height = 850 * @height_scale
 
     # Sort Options UI
-    @sort_font = Gosu::Font.new(300)
-    @current_sort_option
-
-
-    @scroll_y = 0
-    @max_scroll = @albums.size * (@desired_height + 50) - HEIGHT
-    @scrollbar_height = HEIGHT * (HEIGHT.to_f / (@albums.size * (@desired_height + 50)))
+    @scroll_y = 200
+    @max_scroll = 1000*@albums.size/3
+    @scrollbar_height = HEIGHT/@albums.size*3
 
     @current_color_index = 0    
     @color_change_delay = 0    
@@ -89,8 +94,9 @@ class MusicPlayerMain < Gosu::Window
     @total_duration = 0 
 
 
-  end
 
+  end
+  
   def load_albums
     file = File.new('albums.txt', 'r')
     albums = []
@@ -151,50 +157,57 @@ class MusicPlayerMain < Gosu::Window
     next_scale_x = ui_width.to_f / @ui.next.width
     next_scale_y = ui_height.to_f / @ui.next.height
 
+    # Draw UI background
+    Gosu.draw_rect(50, 250, WIDTH-1900, 300, Gosu::Color::GRAY, ZOrder::UI)
+
     # Draw buttons with scaling
     @ui.stop.draw(50, ui_y_position, ZOrder::UI, stop_scale_x, stop_scale_y)
     @ui.play.draw(250 + ui_width + 20, ui_y_position, ZOrder::UI, play_scale_x, play_scale_y)  # Adjust spacing between buttons
     @ui.next.draw(450 + (ui_width + 20) * 2, ui_y_position, ZOrder::UI, next_scale_x, next_scale_y)
 
-    #Draw sort buttons
-    @sort_font.draw_text("Sort By:", 50, 300, ZOrder::UI, 0.7, 0.7, Gosu::Color::WHITE)
-    @sort_font.draw_text("|", 700, 230, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-    @sort_font.draw_text("Title", 1000, 250, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-    @sort_font.draw_text("|", 1700, 230, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-    @sort_font.draw_text("Genre", 1900, 250, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-    @sort_font.draw_text("|", 2700, 230, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
-    @sort_font.draw_text("Date", 2900, 250, ZOrder::UI, 1.0, 1.0, Gosu::Color::WHITE)
+    # Draw the sort text images
+    @ui.sort_by.draw(50, 300, ZOrder::UI, 1.0, 1.0)
+    @ui.pipe.draw(800, 230, ZOrder::UI, 1.0, 1.0)
+    @ui.title.draw(1000, 250, ZOrder::UI, 1.0, 1.0)
+    @ui.pipe.draw(1700, 230, ZOrder::UI, 1.0, 1.0)
+    @ui.genre.draw(1900, 250, ZOrder::UI, 1.0, 1.0)
+    @ui.pipe.draw(2700, 230, ZOrder::UI, 1.0, 1.0)
+    @ui.date.draw(2900, 250, ZOrder::UI, 1.0, 1.0)
 
 
   end
 
   def draw_albums
-		space_between_albums = 200
-		adjust_album_width = 0
-    x = -(@desired_width +space_between_albums) + 300
-		y = 600
+    space_between_albums = 200
+    x = -(@desired_width + space_between_albums) + 300
+    y = 600
     row_spacing = 700
-		@albums.each_with_index do |album, index|
-			x += @desired_width + space_between_albums
-			if x + @desired_width > WIDTH - 1800
-				x = 300
+
+    @albums.each_with_index do |album, index|
+      x += @desired_width + space_between_albums
+      if x + @desired_width > WIDTH - 1800
+        x = 300
         y += @desired_height + row_spacing
-			end
+      end
 
-			original_width = album.artwork.width
-			original_height = album.artwork.height
-			artwork_width_scale = @desired_width.to_f / original_width
-			artwork_height_scale = @desired_height.to_f / original_height
-			album.x = x
-			album.y = y-@scroll_y
+      original_width = album.artwork.width
+      original_height = album.artwork.height
+      artwork_width_scale = @desired_width.to_f / original_width
+      artwork_height_scale = @desired_height.to_f / original_height
+      album.x = x
+      album.y = y - @scroll_y
 
-			if album.y.between?(-@desired_width-200, HEIGHT)
-        truncated_title = truncate_text(album.title.strip,18)
-        @title.draw_text(truncated_title, album.x+50, album.y + @desired_width+200, ZOrder::PLAYER, 3.0 * @width_scale, 3.0 * @height_scale, Gosu::Color::BLACK)
+      if album.y.between?(-@desired_width-200, HEIGHT)
+        # Using Gosu::Image.from_text instead of Gosu::Font for title rendering
+        truncated_title = truncate_text(album.title.strip, @title_size, @desired_width + 200)
+        title_image = Gosu::Image.from_text(truncated_title, @title_size)
+        title_image.draw(album.x + 50, album.y + @desired_width + 200, ZOrder::PLAYER)
+        
         album.artwork.draw(album.x, album.y, ZOrder::PLAYER, artwork_width_scale, artwork_height_scale)
       end
-		end
+    end
   end
+
 
   def draw_track(album)
     colors = [
@@ -215,22 +228,28 @@ class MusicPlayerMain < Gosu::Window
       Gosu::Color.rgba(255, 105, 180, 255) # Hot Pink
     ]
     current_color = colors[@current_color_index]
-    
-    for i in 0..album.tracks.length-1
-      album.tracks[i].x = WIDTH - 1700
-      album.tracks[i].y = 30 + i * 600
+
+    album.tracks.each_with_index do |track, i|
+      track.x = WIDTH - 1700
+      track.y = 30 + i * 600
+
+      track_title_trim = truncate_text(track.title.strip, @track_font_size, WIDTH - track.x)
       
-      if album.tracks[i].song.playing? 
-        @current_playing_track = album.tracks[i].title
-        @track_font.draw_text(album.tracks[i].title, album.tracks[i].x, album.tracks[i].y, ZOrder::PLAYER, 1.0, 1.0, current_color)
+      # Create image from text
+      track_image = Gosu::Image.from_text(track_title_trim, @track_font_size)
+      if track.song.playing?
+        @current_playing_track = track.title
+        track_image.draw(track.x, track.y, ZOrder::PLAYER, 1.0, 1.0, current_color)
       else
-        @track_font.draw_text(album.tracks[i].title, album.tracks[i].x, album.tracks[i].y, ZOrder::PLAYER, 1.0, 1.0, Gosu::Color::BLACK)
+        track_image.draw(track.x, track.y, ZOrder::PLAYER, 1.0, 1.0, Gosu::Color::BLACK)
       end
     end
 
     if @current_playing_track
-      @playing_track_font.draw_text("Playing: #{@current_playing_track}", 1000, 100, ZOrder::PLAYER, 1.0, 1.0, current_color)
-    end 
+      playing_text = "Playing: #{@current_playing_track}"
+      playing_image = Gosu::Image.from_text(playing_text, @playing_track_font_size)
+      playing_image.draw(1000, 100, ZOrder::PLAYER, 1.0, 1.0, current_color)
+    end
 
     # Update the color every 20 frames
     if @color_change_delay > 20
@@ -240,6 +259,7 @@ class MusicPlayerMain < Gosu::Window
       @color_change_delay += 1  # Increment the delay counter
     end
   end
+
 
   def draw_duration_bar
     return unless @current_playing_track && @total_duration > 0
@@ -271,22 +291,14 @@ class MusicPlayerMain < Gosu::Window
     Gosu.draw_rect(WIDTH - 1900, scrollbar_y, 100, @scrollbar_height, Gosu::Color::GRAY, ZOrder::UI)
   end
 
-  def truncate_text(text, max_width)
-    if text.length > max_width
-      while text.length+3 > max_width
-        text = text.chop
-      end
-      text += "..."
+  def truncate_text(text, font_size, width)
+    max_chars = (width / Gosu::Image.from_text('a', font_size).width).to_i
+    if text.length > max_chars
+      text = text[0...max_chars] + "..."
     end
-    return text
+    text
   end
-  # Formats time in milliseconds to "mm:ss"
-  def format_time(milliseconds)
-    total_seconds = milliseconds / 1000
-    minutes = total_seconds / 60
-    seconds = total_seconds % 60
-    format("%02d:%02d", minutes, seconds)
-  end
+
   def play_track(track)
     track.song.play(false)
     @start_time = Gosu.milliseconds
@@ -322,13 +334,13 @@ class MusicPlayerMain < Gosu::Window
     when Gosu::MsLeft
 
       # Check if the sort buttons are clicked
-      if area_clicked(1000, 250, 1000+@sort_font.text_width("Names"), 250+@sort_font.height)  # Title sort button bounds
+      if area_clicked(1000, 250, 1000+@sort_font.text_width("Names"), 250+@sort_font.height)
         sort_choice = "Names"
         puts "Sorting by Title"
-      elsif area_clicked(1900, 250, 1900+@sort_font.text_width("Genre"), 250+@sort_font.height)  # Genre sort button bounds
+      elsif area_clicked(1900, 250, 1900+@sort_font.text_width("Genre"), 250+@sort_font.height)
         sort_choice = "Genre"
         puts "Sorting by Genre"
-      elsif area_clicked(2900, 250, 2900+@sort_font.text_width("Date"), 250+@sort_font.height)  # Date sort button bounds
+      elsif area_clicked(2900, 250, 2900+@sort_font.text_width("Date"), 250+@sort_font.height)
         sort_choice = "Year"
         puts "Sorting by Year"
       end
@@ -347,7 +359,6 @@ class MusicPlayerMain < Gosu::Window
           if area_clicked(track.x, track.y, track.x+@track_font.text_width(track.title), track.y+@track_font.height)
             puts "Track clicked: #{track.title.strip}"
             play_track(track)
-            # Further action can be implemented here, such as playing the track
           end
         end
       end
